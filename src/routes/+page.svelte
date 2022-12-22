1753e70648c6b64adb62bc5889b2c4e7d6b0489f
Alicia Sykes <alicia@omg.lol>
2022-12-22 18:59:15 +0000

    chore: Scafolded empty project
    Setup Svelte, TypeScript, Vite, SASS, prettier, eslint, pnpm, etc

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1 class="border-zinc-100 bg-blue-500">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+    h1 {
+        color: red;
+        // @apply bg-red-500 #{!important};
+    }
+    p {
+        a {
+            color: yellow;
+        }
+    }
+</style>
