c97862b6237d0ad3a7321649f54ffa96a6312917
Alicia Sykes <alicia@omg.lol>
2022-12-22 20:37:42 +0000

    chore: Set, and apply prettier lint rules
    Use 2x spaces for indentation, single quotes, trailing comma, and max line width of 80. Specified in .prettierrc, and applied throughout all the files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <h1 class="border-zinc-100 bg-blue-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
 
 <style lang="scss">
-    h1 {
-        color: red;
-        // @apply bg-red-500 #{!important};
+  h1 {
+    color: red;
+    // @apply bg-red-500 #{!important};
+  }
+  p {
+    a {
+      color: yellow;
     }
-    p {
-        a {
-            color: yellow;
-        }
-    }
+  }
 </style>
