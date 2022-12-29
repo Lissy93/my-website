a03df408a41a77c8f10c8fc7b850157d6fe84b91
Alicia Sykes <alicia@omg.lol>
2022-12-29 13:28:49 +0000

    feat: Adds a store for keeping track of blog posts

---
+++ src/store/BlogStore.ts
@@ -0,0 +1,32 @@
+import { writable, get } from 'svelte/store';
+import { XMLParser } from 'fast-xml-parser';
+
+import type { RssPosts, RssResponse } from '../types/RssXml';
+
+export const blogStore = writable<RssPosts>([]);
+
+const fetchPosts = () => {
+  // Uses fast-xml-parser to convert XML RSS feel into JSON format
+  const parseXml = (rawRssData: string): RssResponse => {
+    const parser = new XMLParser();
+    return parser.parse(rawRssData);
+  };
+
+  // Fetches XML from given URL, calls parse, returns promised post list
+  const fetchRss = async (rssUrl: string) => {
+    return fetch(rssUrl)
+      .then((response) => response.text())
+      .then((rawXml) => parseXml(rawXml).rss.channel.item);
+  };
+
+  // Kick off request and process parsed response
+  const RSS_URL = `https://notes.aliciasykes.com/feed`;
+  const posts = fetchRss(RSS_URL);
+  return posts;
+};
+
+if (get(blogStore).length < 1) {
+  fetchPosts().then((response) => {
+    blogStore.set(response);
+  });
+}
