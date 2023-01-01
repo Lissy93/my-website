31a5134f89c0aff4f458642a1cfb9b3b9061bed1
Alicia Sykes <alicia@omg.lol>
2023-01-01 22:36:56 +0000

    ref: Refactors and improves searchbar markup

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import { searchTerm } from '$src/store/BlogStore';
 
+  // Reference used for focusing on keydown from parent
+  export let searchInputRef = null;
+
+  // Define var to store search term locally, and bind it to store
   let usersSearchTerm = '';
   $: searchTerm.set(usersSearchTerm);
+
+  // Clear search field, if search term is removed elsewhere
   searchTerm.subscribe((changed) => {
     if (!changed) usersSearchTerm = '';
   });
@@ -10,7 +16,7 @@
 
 <form class="filter-form">
   <label for="filter-input">Filter</label>
-  <input bind:value={usersSearchTerm} type="text" id="filter-input">
+  <input bind:value={usersSearchTerm} type="text" id="filter-input" bind:this={searchInputRef} >
 </form>
 
 <style lang="scss">
@@ -19,7 +25,7 @@ form {
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  margin-right: calc(5vw + 1rem);
+  margin: 0.25rem calc(5vw + 1rem);
 }
 label {
   margin-right: 1rem;
