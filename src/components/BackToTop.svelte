6b142b9b3810828a52a2af7f29ba4c24cf42d85b
Alicia Sykes <alicia@omg.lol>
2023-01-01 01:50:19 +0000

    feat: Adds a button to scroll back to top

---
+++ src/components/BackToTop.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  export let showOnPx = 150;
+  let hidden = true;
+
+  function goTop() {
+    document.body.scrollIntoView();
+  }
+
+  function scrollContainer() {
+    return document.documentElement || document.body;
+  }
+
+  function handleOnScroll() {
+    if (!scrollContainer()) {
+      return;
+    }
+
+    if (scrollContainer().scrollTop > showOnPx) {
+      hidden = false;
+    } else {
+      hidden = true;
+    }
+  }
+</script>
+
+<svelte:window on:scroll={handleOnScroll} />
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="back-to-top" on:click={goTop} class:hidden>↑ Back to top</div>
+
+<style lang="scss">
+  .back-to-top {
+    opacity: 1;
+    z-index: 5;
+    position: fixed;
+    cursor: pointer;
+    user-select: none;
+    transition: opacity 0.5s, visibility 0.5s;
+    right: 1rem;
+    bottom: 1rem;
+    color: var(--background);
+    background: var(--accent-2);
+    padding: 0.25rem 0.5rem;
+    border-radius: 4px;
+    font-weight: bold;
+    font-family: RedHatText;
+    &.hidden {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+</style>
