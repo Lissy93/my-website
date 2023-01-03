030d43c276b8d0750d6915cef19c6e9a3d029582
Alicia Sykes <alicia@omg.lol>
2023-01-03 22:42:32 +0000

    feat: Built simple navbar, with dynamic links and styles

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,38 +1,57 @@
 <script lang="ts">
-  export let size = '24';
-  export let color = 'currentColor';
-  export let variation: 'solid' | 'outline' = 'outline';
-  let viewBox: string;
-  let svgpath: string;
-  let svgoutline = `<path d="M3.75 6.75H20.25M3.75 12H20.25M3.75 17.25H20.25" stroke="${color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> `;
-  let svgsolid = `<path fill-rule="evenodd" clip-rule="evenodd" d="M3 6.75C3 6.33579 3.33579 6 3.75 6H20.25C20.6642 6 21 6.33579 21 6.75C21 7.16421 20.6642 7.5 20.25 7.5H3.75C3.33579 7.5 3 7.16421 3 6.75ZM3 12C3 11.5858 3.33579 11.25 3.75 11.25H20.25C20.6642 11.25 21 11.5858 21 12C21 12.4142 20.6642 12.75 20.25 12.75H3.75C3.33579 12.75 3 12.4142 3 12ZM3 17.25C3 16.8358 3.33579 16.5 3.75 16.5H20.25C20.6642 16.5 21 16.8358 21 17.25C21 17.6642 20.6642 18 20.25 18H3.75C3.33579 18 3 17.6642 3 17.25Z" fill="${color}"/> `;
-  $: switch (variation) {
-    case 'outline':
-      svgpath = svgoutline;
-      viewBox = '0 0 24 24';
-      break;
-    case 'solid':
-      svgpath = svgsolid;
-      viewBox = '0 0 24 24';
-      break;
-    default:
-      svgpath = svgoutline;
-      viewBox = '0 0 24 24';
-  }
-  export let ariaLabel = 'bars 3';
+  import { page } from '$app/stores';
+  export const navLinks = [
+    { title: 'Home', destination: '/' },
+    { title: 'Blog', destination: '/blog' },
+    { title: 'Projects', destination: '/projects' },
+    { title: 'About', destination: '/about' },
+    { title: 'Contact', destination: '/contact' },
+  ];
 </script>
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width={size}
-  height={size}
-  class={$$props.class}
-  {...$$restProps}
-  aria-label={ariaLabel}
-  fill="none"
-  {viewBox}
-  stroke-width="2"
-  on:click
->
-  {@html svgpath}
-</svg>
+<nav>
+  <a class="homepage-link" href="/">
+    <h1>Website</h1>
+  </a>
+  <div class="nav-links">
+    {#each navLinks as navLink }
+    <a
+      href={navLink.destination}
+      data-sveltekit-preload-data="hover"
+      class:active={$page.url.pathname === navLink.destination}>{navLink.title}</a>
+    {/each}
+  </div>
+</nav>
+
+<style lang="scss">
+  nav {
+    display: flex;
+    justify-content: space-between;
+    background: var(--card-background);
+    border-bottom: var(--card-border);
+    color: var(--foreground);
+    padding: 0.5rem;
+    a.homepage-link {
+      text-decoration: none;
+      text-align: center;
+      h1 {
+        margin: 0;
+        color: var(--foreground);
+      }
+    }
+    .nav-links a {
+      font-weight: 700;
+      font-size: 1.2rem;
+      color: var(--foreground);
+      text-decoration: none;
+      border-radius: 4px;
+      padding: 0.25rem 0.5rem;
+      margin: 0.25rem;
+      text-align: center;
+      &.active, &:hover {
+        background: var(--accent-2);
+        color: var(--background);
+      }
+    }
+  }
+</style>
