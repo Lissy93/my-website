d0fc6a71c78c92f20f64afd882eb683c698610eb
Alicia Sykes <alicia@omg.lol>
2023-01-04 23:48:15 +0000

    wip: Adds template for Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<footer>
+  Hello!
+</footer>
+
+<!--
+  TODO Add:
+  - Link to source code
+  - Copyright, license name
+  - Page links
+  - More links
+-->
+
+<style lang="scss">
+  footer {
+    bottom: 0;
+    // position: fixed;
+    padding: 0.5rem 1rem;
+    width: calc(100vw - 2rem);
+    background: var(--card-background);
+  }
+</style>
