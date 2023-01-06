f3713b51e8d729ad7acfa21585c16ca63dc30b21
Alicia Sykes <alicia@omg.lol>
2023-01-06 02:52:44 +0000

    wip: Formats to homepage to show list of routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,89 @@
 <script>
+  import { get } from 'svelte/store';
   import { blogStore } from '$src/store/BlogStore';
+  import Loading from '$src/components/Loading.svelte';
+
+  export const tiles = [
+    { title: 'Blog', link: '/blog' },
+  ];
+
+  export let showLoader = false;
+
+  export const checkIfLoadNeeded = () => {
+    if (get(blogStore).length === 0) {
+      showLoader = true;
+    }
+  };
+
 </script>
 
-<h2>Blog Post Count: {$blogStore.length}</h2>
+<div class="hero">
+  <h1>Alicia Sykes</h1>
+</div>
 
-<h1 class="border-zinc-100 bg-blue-500">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+{#if showLoader}
+  <Loading message="Pre-fetching Posts" />
+{/if}
+
+{#if !showLoader}
+  <div class="tiles">
+    <a class="tile" href="/blog" on:click={checkIfLoadNeeded}>
+      <h3>Blog</h3>
+    </a>
+    <a class="tile" href="/projects">
+      <h3>Projects</h3>
+    </a>
+    <a class="tile" href="/about">
+      <h3>About</h3>
+    </a>
+    <a class="tile" href="/contact">
+      <h3>Contact</h3>
+    </a>
+  </div>
+{/if}
 
 <style lang="scss">
 
 :global(html) {
   scroll-behavior: smooth;
+}
+
+.hero {
+  text-align: center;
+  h1 {
+    font-size: 4rem;
+    cursor: default;
+  }
+}
+
+.tiles {
+  display: grid;
+  max-width: 70rem;
+  margin: auto;
+  padding: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  gap: 1rem;
+  a.tile {
+    color: var(--foreground);
+    border: var(--card-border);
+    background: var(--card-background);
+    border-radius: 4px;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    border-left: 4px solid var(--accent-2);
+    transition: all ease-in-out 0.25s, transform ease-in-out 0.3s;
+    h3 {
+      font-size: 2rem;
+      transition: all .25s ease-in-out;
+    }
+    &:hover {
+      border-left-width: 8px;
+      transform: scale(1.02);
+      h3 {
+        color: var(--accent-2);
+      }
+    }
+  }
 }
 
 </style>
