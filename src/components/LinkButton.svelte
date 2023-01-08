2aa50e9fbc30823ae1ad3d1eaf999bf59966636a
Alicia Sykes <alicia@omg.lol>
2023-01-08 20:07:00 +0000

    feat: Simple link button component

---
+++ src/components/LinkButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let to = '';
+</script>
+
+<a href={to}>
+  <slot />
+</a>
+
+<style>
+a {
+  text-decoration: none;
+  color: var(--background);
+  background: var(--accent);
+  padding: 0.25rem 0.5rem;
+  margin: 0.5rem;
+  border: 0;
+  border-radius: 4px;
+  cursor: pointer;
+  font-family: RedHatText;
+  font-weight: bold;
+  font-size: 1rem;
+}
+</style>
