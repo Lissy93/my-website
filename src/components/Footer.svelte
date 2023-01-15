04ea0bdada68170c7d954801fed1ad270d072ec3
Alicia Sykes <alicia@omg.lol>
2023-01-15 21:53:05 +0000

    feat: Added content to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,21 +1,36 @@
-<footer>
-  Hello!
-</footer>
+<script lang="ts">
+  import { config } from '$src/store/BlogStore';
+  const { footerInfo, source } = config;
+  export let color: string;
+</script>
 
-<!--
-  TODO Add:
-  - Link to source code
-  - Copyright, license name
-  - Page links
-  - More links
--->
+<footer style={`--accent: ${color};`}>
+  <p>
+  © <a href={footerInfo.authorSite}>{footerInfo.author}</a> {new Date().getFullYear()}
+  Licensed under <a href={footerInfo.licenseLink}>{footerInfo.license}</a> -
+  View on <a href={source}>GitHub</a>
+  </p>
+</footer>
 
 <style lang="scss">
   footer {
     bottom: 0;
-    // position: fixed;
     padding: 0.5rem 1rem;
     width: calc(100vw - 2rem);
     background: var(--card-background);
+    p {
+      margin: 0;
+      text-align: center;
+      a {
+        color: var(--accent);
+        border-radius: 4px;
+        padding: 0.1rem 0.25rem;
+        text-decoration: none;
+        &:hover {
+          background: var(--accent);
+          color: var(--background);
+        }
+      }
+    }
   }
 </style>
