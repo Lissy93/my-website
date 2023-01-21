cf28e3c3cb529a4dca4cff41945e035800150d6b
Alicia Sykes <alicia@omg.lol>
2023-01-21 23:46:56 +0000

    types: Adds typescript interface for GH repository

---
+++ src/types/Project.ts
@@ -0,0 +1,19 @@
+
+export interface Project {
+  id: number;
+  name: string;
+  user: string;
+  url: string;
+  description: string;
+  isFork: boolean;
+  createdAt: string;
+  updatedAt: string;
+  homepage: string;
+  language: string;
+  license: string;
+  size: number;
+  stars: number;
+  forks: number;
+  issues: number;
+  topics: string[];
+};
