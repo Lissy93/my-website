888b0ca24f9e6545b08118146ac765c5fdbc3444
Alicia Sykes <alicia@omg.lol>
2023-01-28 15:35:51 +0000

    types: Adds interfaces for social media data

---
+++ src/types/Socials.ts
@@ -0,0 +1,42 @@
+
+export const SupportedSocials: readonly string[] = [
+  'Twitter',
+  'GitHub',
+  'Dev.to',
+  'Mastodon',
+  'LinkedIn',
+  'Instagram',
+  'YouTube',
+  'Reddit',
+  'StackOverflow',
+  'KeyBase',
+  'Facebook',
+  'PeerList',
+] as const;
+
+export interface SocialNetwork {
+  name: typeof SupportedSocials[number];
+  icon: string;
+  tone: string;
+  link: string;
+  noAt?: boolean;
+};
+
+export interface SocialMetric {
+  label: string;
+  value: string | number;
+};
+
+export interface UserSocial extends SocialNetwork {
+  user: string;
+  metrics?: SocialMetric[];
+};
+
+export type Usernames = {
+  [key in typeof SupportedSocials[number]]: string;
+};
+
+export interface SocialMetric {
+  label: string;
+  value: string | number;
+};
