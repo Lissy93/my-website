60a96035bef74a5ac03d56de39154fca0f3043d1
Alicia Sykes <alicia@omg.lol>
2023-01-30 21:18:18 +0000

    wip: starts building a heading component

---
+++ src/components/Heading.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+type Level = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+export let level: Level = 'h1';
+</script>
+
+<svelte:element this={level} class="title">
+  <slot></slot>
+</svelte:element>
+
+
+<style lang="scss">
+  .title {
+    color: var(--accent);
+    &h1 {
+      font-size: 2rem;
+      margin: 1rem 0;
+    }
+  }
+</style>
