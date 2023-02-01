317b1943270de58ff550dc55327eaf871102c60a
Alicia Sykes <alicia@omg.lol>
2023-02-01 20:16:42 +0000

    feat: Adds simple about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,105 @@
 <script lang="ts">
   import Heading from '$src/components/Heading.svelte';
 
+  import config from '$src/helpers/config';
+
+  const { intro, bio, picture } = config.about; 
+
+  const morePages = config.routeLinks.filter((link) => !['/about', '/'].includes(link.route));
+
 </script>
 
-<Heading level="h1">Heading H1</Heading>
-<Heading level="h2">Heading H2</Heading>
-<Heading level="h3">Heading H3</Heading>
-<Heading level="h4">Heading H4</Heading>
-<Heading level="h5">Heading H5</Heading>
-<Heading level="h6">Heading H6</Heading>
+<div class="heading">
+  <Heading level="h2" size="2.5rem">About</Heading>
+</div>
+
+<div class="content">
+
+  <section class="intro">
+    <i>{intro}</i>
+    {#each bio as bioLine}
+      <p class="bio-line">{@html bioLine}</p>
+    {/each}  
+  </section>
+
+  <section class="ugly-picture">
+    <img src={picture} alt="Users profile" />
+    <div class="pages">
+      {#each morePages as page}
+        <a href={page.route} style={`--page-color: ${page.color}`}>{page.label}</a>
+      {/each}
+    </div>
+  </section>
+
+</div>
+
 
 <style lang="scss">
+.heading {
+  margin: 1rem calc(5vw + 1rem) 0;
+}
+
+.content {
+  display: flex;
+  gap: 1rem;
+  margin: 1rem auto;
+  max-width: calc(90% - 1rem);
+}
+
+section {
+  padding: 1rem;
+  border-radius: var(--curve-factor);
+  background: var(--card-background);
+  &.intro {
+    grid-column-start: span 3;
+    grid-row-start: span 2;
+    i {
+      opacity: 0.8;
+    }
+    :global(p) { 
+      margin: 1rem 0;
+      font-size: 1.25rem;
+      line-height: 1.8rem;
+      font-family: RedHatText;
+    }
+    :global(a) {
+      color: var(--accent);
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+  &.ugly-picture {
+    max-width: 300px;
+    height: fit-content;
+    text-align: center;
+    img {
+      width: 100%;
+      margin: 0 auto;
+      border-radius: var(--curve-factor);
+    }
+  }
+
+  .pages {
+    margin: 1rem 0;
+    opacity: 0.7;
+    &:hover { opacity: 1; }
+    a {
+      color: var(--page-color, var(--accent));
+      border-radius: var(--curve-factor);
+      padding: 0.1rem 0.25rem;
+      margin: 0.5rem;
+      font-weight: bold;
+      text-decoration: none;
+      transition: all ease-in-out 0.2s;
+      &:hover {
+        color:  var(--background);
+        background:  var(--page-color, var(--accent));
+      }
+    }
+  }
+  
+}
+
 </style>
