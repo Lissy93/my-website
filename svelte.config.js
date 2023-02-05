4078c9da9f18ae61dd57563732a22a3f96281302
Alicia Sykes <alicia@omg.lol>
2023-02-05 19:02:30 +0000

    deploy: Adds multi-adapter logic, and Netlify config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const multiAdapter = (adapters) => {
+  return {
+    async adapt(argument) {
+      await Promise.all(adapters.map(item =>
+        Promise.resolve(item).then(resolved => resolved.adapt(argument))
+      ))
+    }
+  };
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
+    adapter: multiAdapter([autoAdapter(), netlifyAdapter()]),
     alias: {
       '$src/*': 'src/*',
     },
