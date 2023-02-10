d590dde0d1b506a880628dd4861a1ab40d900a37
Alicia Sykes <alicia@omg.lol>
2023-02-10 20:51:06 +0000

    styles: Use variables for accent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { get } from 'svelte/store';
   import BackToTop from '$src/components/BackToTop.svelte';
   import NavBar from '$src/components/NavBar.svelte';
   import Footer from '$src/components/Footer.svelte';
   import { fade } from 'svelte/transition';
   import { page } from '$app/stores';
-  import { config } from '$src/store/BlogStore';
-
+  import { config, theme } from '$src/store/BlogStore';
+  import type { Theme } from '$src/types/Config';
+  import { onDestroy } from 'svelte';
+  
   const routeBasedAccent = (pathname?: string) => {
     const path = pathname || $page.url.pathname;
     return config.routeLinks?.find((rc) => rc.route === path)?.color || '#ff0099';
@@ -15,10 +19,43 @@
     const route = config.routeLinks.find((rl) => rl.route === pathname);
     return route ? `${route.label} | ${config.title}` : config.title;
   };
+
+  const encodeB64 = (str: string) => {
+    return browser ? window.btoa(str) : Buffer.from(str, 'utf-8').toString('base64');
+  }
+
+  /* Based on the currently selected theme, make CSS variables from color config */
+  const makeCSSVars = (themeToUse: string) => {
+    let results = '';
+    const themeData = ((config.colorSchemes || {})[themeToUse]) || {};
+    Object.keys(themeData).forEach((varName: string) => {
+      results += `--${varName}:${themeData[varName]} !important; `;
+    });
+    return `:root { ${results} }`;
+  }
+
+  const makeEncodedCSS = (t: Theme) => {
+    return `data:text/css;base64,${encodeB64(makeCSSVars(t))}`;
+  }
+
+  let encodedCss = makeEncodedCSS($theme);
+
+  theme.subscribe(() => {
+    encodedCss = makeEncodedCSS($theme);
+  });
+
+  onDestroy(() => {
+    console.log('Layout destroyed');
+  });
+
+
 </script>
 
 <svelte:head>
     <title>{makeTitle($page.url.pathname)}</title> 
+    <meta name="color-scheme" content={$theme} />
+    <!-- <link rel="stylesheet" href="/" /> -->
+    <link rel="stylesheet" href={encodedCss} />
 </svelte:head>
 
 {#if $page.url.pathname !== '/'}
@@ -47,6 +84,7 @@
   @import "$src/styles/media-queries.scss";
   @import "$src/styles/typography.scss";
   @import "$src/styles/dimensions.scss";
+  @import "$src/styles/themes.scss";
 
   main {
     min-height: 100%;
@@ -93,4 +131,11 @@
     background-color: var(--accent);
     color: var(--background);
   }
-</style>
+
+//   :global([data-theme='light']) {
+//     @include light-theme;
+//   }
+//   :global([data-theme='callisto']) {
+//     @include callisto-theme;
+//   }
+// </style>
