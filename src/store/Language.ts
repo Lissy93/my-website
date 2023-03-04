def63d3c4736465e7034b01c33b046970d4a7548
Alicia Sykes <alicia@omg.lol>
2023-03-04 22:34:33 +0000

    feat: Implements language switching functionality

--- src/store/Language.ts
+++ src/store/Language.ts
@@ -1,19 +1,36 @@
+import { browser } from '$app/environment';
 import { derived, writable } from 'svelte/store';
 import type { Locale, Translations } from '$src/types/Language';
-import translations from '$src/helpers/translations';
+import translations, { supportedLanguages } from '$src/helpers/translations';
+import config from '$src/helpers/config';
 
-export const locale = writable<Locale>('en');
+// If no translations for a given locale key are found, fallback to English
+const fallbackLocale: Locale = 'en';
+
+// Gets the initial language, either from local storage (if browser), or from the config
+const initialLocale = browser
+  ? localStorage.getItem('locale') as Locale
+  : null || config.defaultLanguage || 'en';
+
+  // The currently selected language
+export const locale = writable<Locale>(initialLocale);
+
+// Full list of available languages
 export const locales = Object.keys(translations) as Locale[];
 
-const defaultLocale = locales[0];
+// When lang updated, save in localStorage ready for next time
+locale && locale.subscribe((newLocale: Locale) => {
+  if (browser) localStorage.locale = newLocale
+});
 
+// The main translation function, which returns a value for given key and locale
 function translate(locale: Locale, key: string, vars: Translations[Locale]) {
 
   // Throw error if locale not found
   if (!translations[locale]) throw new Error(`No translations found for ${locale}`);
 
   // Get the text from the translations for locale or fallback to default locale
-  let text = translations[locale][key] || translations[defaultLocale][key];
+  let text = translations[locale][key] || translations[fallbackLocale][key];
 
   // Throw error if no translation found
   if (!text) throw new Error(`No translation found for ${locale}.${key}`);
@@ -27,6 +44,12 @@ function translate(locale: Locale, key: string, vars: Translations[Locale]) {
   return text;
 }
 
+// For use within component, with $t('key', { vars }) to return actual text
 export const t = derived(locale, ($locale) => (key: string, vars = {}) =>
   translate($locale, key, vars)
+);
+
+// Returns the meta data for the current language (flag, name, etc)
+export const languageMeta = derived(locale, ($locale) =>
+  supportedLanguages.find((l) => l.code === $locale)
 );
--- src/types/Language.ts
+++ src/types/Language.ts
@@ -1,7 +1,14 @@
-export type Locale = 'en';
+export type Locale = 'en' | 'fr';
 
 export type Translations = {
   [key in Locale]: {
     [key: string]: string;
   };
 };
+
+export interface LanguageMeta {
+  name: string;
+  flag: string;
+  code: Locale;
+  text: Translations[Locale];
+}
