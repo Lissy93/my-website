357eec668b1829b16c48f6212c1ca0d9b27bf466
Alicia Sykes <alicia@omg.lol>
2023-04-08 23:22:22 +0100

    Add basic styling to error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,46 @@
 <script>
   import { page } from '$app/stores';
+  import LinkButton from '$src/components/LinkButton.svelte';
 
   const emojis = {
     // TODO add the rest!
+    401: '🔒',
+    403: '🚫',
     404: '🧱',
     420: '🫠',
     500: '💥',
+    501: '🚧',
   };
 </script>
 
-<h1>{$page.status} {$page.error.message}</h1>
+<div class="errors-as-far-as-the-eye-can-see">
+<h1>
+  <span>{$page.status}</span>
+  <br>
+  {$page.error.message}
+</h1>
+<p>This is sad :'(</p>
 <span style="font-size: 10em">
   {emojis[$page.status] ?? emojis[500]}
 </span>
+<LinkButton to="/">Back Home</LinkButton>
+</div>
+
+<style lang="scss">
+.errors-as-far-as-the-eye-can-see {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+    font-weight: 500;
+    span { font-size: 5rem; }
+  }
+  p {
+    margin: 0.5rem 0;
+    color: var(--dimmed-text);
+  }
+}
+</style>
