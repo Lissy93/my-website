cb907f7113f5d5722321bd242bfbcaecf28b2357
Alicia Sykes <alicia@omg.lol>
2023-04-11 20:05:09 +0100

    Move LOAD_INDEX to public env var

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
 import config from '$src/helpers/config';
-import { LOAD_INDEX } from '$env/static/private';
+import * as publicEnvs from '$env/static/public';
 
 // If the app is configured to use the /index route instead of home, then redirect
 export function load() {
-  if ((LOAD_INDEX && LOAD_INDEX === 'true') || config?.loadIndex) {
+  const shouldLoadIndex = publicEnvs?.PUBLIC_LOAD_INDEX || config?.loadIndex;
+  if (shouldLoadIndex) {
     throw redirect(302, '/index');
   }
 };
