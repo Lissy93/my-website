1549bda9e2d311f42e186047dad4c234100bfaf9
Alicia Sykes <alicia@omg.lol>
2023-08-27 20:37:58 +0100

    Updates svelte config, to use specified adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,37 @@
 import autoAdapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
 import netlifyAdapter from '@sveltejs/adapter-netlify';
 import vercelAdapter from '@sveltejs/adapter-vercel';
 import nodeAdapter from '@sveltejs/adapter-node';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const multiAdapter = (adapters) => {
-  return {
-    async adapt(argument) {
-      await Promise.all(
-        adapters.map((item) =>
-          Promise.resolve(item).then((resolved) => resolved.adapt(argument))
-        )
-      );
-    },
-  };
-};
+let selectedAdapter;
+
+if (process.env.DEPLOY_TARGET === 'NETLIFY') {
+  selectedAdapter = netlifyAdapter();
+} else if (process.env.DEPLOY_TARGET === 'VERCEL') {
+  selectedAdapter = vercelAdapter();
+} else if (process.env.DEPLOY_TARGET === 'NODE') {
+  selectedAdapter = nodeAdapter();
+} else if (process.env.DEPLOY_TARGET === 'AUTO') {
+  selectedAdapter = autoAdapter();
+} else {
+  selectedAdapter = staticAdapter();
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: multiAdapter([
-      autoAdapter(),
-      netlifyAdapter(),
-      vercelAdapter(),
-      nodeAdapter(),
-    ]),
+    adapter: selectedAdapter,
     alias: {
       '$src/*': 'src/*',
     },
+    prerender: {
+      handleMissingId: 'ignore', // or 'remove' or 'fail'
+      crawl: true,
+    }
   },
 };
 
