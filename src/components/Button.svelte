9316843659242bc9e912231f5503d718f7ef1803
Alicia Sykes <alicia@omg.lol>
2023-08-27 16:39:42 +0100

    Adds pagination to projects screen

---
+++ src/components/Button.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import Icon from '$src/components/Icon.svelte';
+
+  export let priority = 'primary';
+  export let textColor = '';
+  export let icon: string | null = null;
+  export let click: () => void = () => {};
+</script>
+
+<button class={priority} style={`--textColor: ${textColor};`} on:click={click}>
+  {#if icon}
+    <Icon name={icon} class={priority} color={textColor} />
+  {/if}
+  <slot />
+</button>
+
+<style lang="scss">
+  button {
+    text-decoration: none;
+    padding: 0.25rem 0.5rem;
+    margin: 0;
+    border: 0;
+    border-radius: 4px;
+    cursor: pointer;
+    font-family: RedHatText;
+    font-size: 1.2rem;
+    filter: grayscale(15%);
+    transition: all 0.2s ease-in-out;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    &:hover {
+      filter: grayscale(0%);
+      transform: scale(1.05) rotate(1deg);
+    }
+    &.primary {
+      background: var(--accent);
+      color: var(--background);
+      font-weight: bold;
+    }
+    &.secondary {
+      background: var(--card-background);
+      color: var(--foreground);
+      border: var(--card-border);
+      &:hover {
+        transform: scale(1.04) rotate(-0.8deg);
+      }
+    }
+    &.outline {
+      background: var(--background);
+      color: var(--accent);
+      border: 1px solid var(--accent);
+      &:hover {
+        transform: scale(1.04) rotate(-0.8deg);
+        background: var(--accent);
+        color: var(--background);
+        :global(svg) {
+          fill: var(--background);
+        }
+      }
+    }
+    color: var(--textColor, currentcolor);
+  }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,6 +2,7 @@
   import { browser } from '$app/environment';
   import { slide } from 'svelte/transition';
   import Heading from '$src/components/Heading.svelte';
+  import Button from '$src/components/Button.svelte';
   import ProjectCard from './ProjectCard.svelte';
   import MirrorCard from './MirrorCard.svelte';
   import ProjectRow from './ProjectRow.svelte';
@@ -19,6 +20,7 @@
   const repos = data.repos;
   const mirrors = data.mirrors;
   let searchInputRef: any | HTMLElement; // Has to be any, as used in context of <svelte:element>
+  let itemsToShow = 20;
   
   let sortBy: string = '';
   let orderBy: string = '';
@@ -69,6 +71,8 @@
     hiddenRepos = [];
     selectedLanguages = [];
     selectedTopics = [];
+
+    itemsToShow = 20;
   };
 
   const toggleShowFilters = (e: any) => {
@@ -108,10 +112,14 @@
         dataItem && dataItem.toLowerCase().includes(searchVal.toLowerCase())
       );
     };
+
+    itemsToShow = repos.length; // Show everything while searching
+
     // Search by name
     filteredRepos = repos.filter((repo: Project) => {
       return doesValInclude(searchTerm, repo.name);
     });
+
     // If no results, then expand search to include description, language and homepage
     if (filteredRepos.length === 0) {
       filteredRepos = repos.filter((repo: Project) => {
@@ -123,6 +131,7 @@
       });
     }
 
+    // Hide repo types if selected by user (forks and archived projects)
     if (hiddenRepos.length > 0) {
       filteredRepos = filteredRepos.filter((repo: Project) => {
         if (hiddenRepos.includes('hide-forks') && repo.isFork) return false;
@@ -131,6 +140,7 @@
       });
     }
 
+    // If sort preference set, then sort results
     if (sortBy) {
       filteredRepos = filteredRepos.sort((a: Project, b: Project) => {
         if (sortBy === 'sort-date') {
@@ -142,12 +152,14 @@
       });
     }
 
+    // If language selected, then filter by language
     if (selectedLanguages.length > 0) {
       filteredRepos = filteredRepos.filter((repo: Project) => {
         return selectedLanguages.includes(repo.language);
       });
     }
 
+    // If topic selected, then filter by topic
     if (selectedTopics.length > 0) {
       filteredRepos = [...filteredRepos.filter((repo: Project) => {
         return repo.topics.some((topic: string) => {
@@ -156,7 +168,7 @@
       })];
     }
 
-
+    // If order preference set, then order results
     if (orderBy) {
       filteredRepos = orderBy === 'order-asc' ? filteredRepos.reverse() : filteredRepos;
     }
@@ -261,7 +273,6 @@
       </div>
     </div>
 
-
     <RadioGroup
       on:change={updateSelectedTopics}
       groupLabel="Topics"
@@ -290,7 +301,7 @@
       >
         <ProjectRow {repo} />
         </AnimateOnScroll>
-      {:else}
+      {:else if (index < itemsToShow)}
         <AnimateOnScroll
         once={true}
         animation={'fade-in 1s cubic-bezier(0.4, 0.6, 0.5, 1.000) both'}
@@ -302,6 +313,16 @@
     {/each}
   </div>
 </section>
+
+{#if repos.length > itemsToShow}
+  <div class="load-more">
+    <span>Showing {itemsToShow} of {repos.length} projects</span>
+    <Button
+      icon="expand-down"
+      click={() => itemsToShow += 20}
+    >Load more</Button>
+  </div>
+{/if}
 
 <section class="mirror-list">
   <div class="title">
@@ -320,172 +341,5 @@
 </section>
 
 <style lang="scss">
-  @import '$src/styles/dimensions.scss';
-  section {
-    margin: 1rem auto;
-    text-align: left;
-    width: 100%;
-  }
-  .title {
-    margin: 0 calc(5vw + 1rem);
-  }
-  .project-grid, .mirror-grid {
-    display: grid;
-    grid-auto-flow: dense;
-    grid-template-columns: repeat(auto-fit, minmax(var(--grid-item-width), 1fr));
-    grid-template-columns: repeat(auto-fit, minmax(var(--grid-item-width), 1fr));
-    gap: var(--grid-item-spacing);
-    padding: var(--grid-item-spacing);
-    margin: var(--grid-item-spacing) 5vw;
-    list-style: none;
-  }
-
-  .post-filter-options {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    .filter-controls {
-      display: flex;
-      gap: 1rem;
-      margin: 0 calc(5vw + 1rem);
-      .filter-toggle-btn {
-        font-family: FiraCode;
-        background: var(--card-background);
-        border: var(--card-border);
-        border-radius: var(--curve-factor);
-        padding: 0.35rem;
-        color: var(--foreground);
-        cursor: pointer;
-      }
-    }
-  }
-
-  .advanced-filters {
-    display: flex;
-    align-items: center;
-    margin: 0 calc(5vw + 1rem);
-    flex-wrap: wrap;
-    .view-mode-label {
-      font-size: 0.8rem;
-      color: var(--dimmed-text);
-    }
-    .view-mode-options {
-      display: flex;
-    }
-    .language-selector {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 0.5rem;
-      align-items: center;
-      .selectedLang {
-        display: flex;
-        border: var(--card-border);
-        padding: 5px 5px 0 5px;
-        border-radius: 4px;
-        &::before {
-          content: '✓';
-          margin-right: 10px;
-        }
-      }
-    }
-    .af {
-      display: flex;
-      flex-direction: column;
-      &.cancel-filters {
-        button {
-          margin: 0 0.25rem;
-          padding: 0.2rem 0.25rem;
-          font-family: FiraCode;
-          background: var(--card-background);
-          border: var(--card-border);
-          border-radius: 4px;
-          cursor: pointer;
-          &:hover { color: var(--accent); }
-        }
-      }
-    }
-  }
-
-  form {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-  }
-  label {
-    margin-right: 1rem;
-  }
-  input {
-    background: var(--card-background);
-    outline: none;
-    border: var(--card-border);
-    padding: 0.25rem 0.5rem;
-    color: var(--accent);
-    font-family: FiraCode;
-    font-size: 1rem;
-    border-radius: 4px;
-  }
-
-  .display-btn {
-    background: var(--card-background);
-    border: var(--card-border);
-    border-radius: var(--curve-factor);
-    margin-right: 0.25rem;
-    display: flex;
-    padding: 0.35rem;
-    color: var(--foreground);
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    &:last-of-type {
-      margin-right: 1rem;
-    }
-    &:hover {
-      color: var(--accent);
-      transform: scale(1.05);
-    }
-    &.selected {
-      // color: var(--accent);
-      border: 1px solid var(--accent);
-    }
-  }
-
-  .results-info {
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    margin: 0.25rem calc(5vw + 1rem);
-    p {
-      margin: 0;
-    }
-    button {
-      border: none;
-      outline: none;
-      background: var(--accent);
-      color: var(--foreground);
-      border-radius: 4px;
-      margin: 0 0.5rem;
-      cursor: pointer;
-    }
-  }
-
-  .no-results {
-    background: var(--card-background);
-    border: var(--card-border);
-    border-radius: 4px;
-    color: var(--foreground);
-    margin: 2rem calc(5vw + 1rem);
-    padding: 1rem 2rem;
-    h4 {
-      font-size: 1.6rem;
-      margin: 0.5rem 0;
-    }
-  }
-
-  @keyframes -global-fade-in {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
+  @import './styles.scss';
 </style>
