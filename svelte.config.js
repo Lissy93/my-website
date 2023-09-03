e1573b4e75556936a126f54083c9424c0893fea1
Alicia Sykes <alicia@omg.lol>
2023-09-03 17:33:20 +0100

    Resolve build hanging on success status

--- svelte.config.js
+++ svelte.config.js
@@ -33,6 +33,21 @@ const config = {
       crawl: true,
     }
   },
+  worker: {
+    plugins: [
+      {
+        name: 'remove-manifest',
+        configResolved(c) {
+          const manifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:manifest');
+          c.worker.plugins.splice(manifestPlugin, 1);
+          const ssrManifestPlugin = c.worker.plugins.findIndex(
+            (p) => p.name === 'vite:ssr-manifest'
+          );
+          c.plugins.splice(ssrManifestPlugin, 1);
+        }
+      }
+    ]
+  }
 };
 
 export default config;
