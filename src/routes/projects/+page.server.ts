c0318d737e48275d9b0a95cf69fcc0cec755f650
Alicia Sykes <alicia@omg.lol>
2024-01-06 12:37:23 +0000

    Only fetch the 100 newest projects

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -40,7 +40,7 @@ const makeProjectList = async (ghResponse: any): Promise<Project[]> => {
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-  const githubApiUrl = `https://api.github.com/users/${config.githubUser}/repos?per_page=100`;
+  const githubApiUrl = `https://api.github.com/users/${config.githubUser}/repos?per_page=100&sort=created&direction=desc`;
   const githubRequest = {
     headers: GITHUB_TOKEN ? { Authorization: `Bearer ${GITHUB_TOKEN}` } : {},
   };
